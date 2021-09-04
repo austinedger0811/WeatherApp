27f9fe7907ea4b4bea176acb01a4834b93adcc8c
Austin M. Edger <ame2194@columbia.edu>
2021-09-04 17:49:25 -0400

    init commit.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  text-align: center;
+  background-color: blue;
+`
+
+export default App;
