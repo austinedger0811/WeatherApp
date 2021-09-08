498de5178497252d63b5964cc69f8f2f4e11db14
Austin M. Edger <ame2194@columbia.edu>
2021-09-08 19:44:59 -0400

    Firebase auto-deploy

--- src/RainChart.js
+++ src/RainChart.js
@@ -6,7 +6,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 
-import { LineChart, Line, ResponsiveContainer, XAxis, } from 'recharts'
+import { LineChart, Line, ResponsiveContainer, XAxis, ReferenceDot } from 'recharts'
 
 const createHour = (value, time) => {
 	return {value, time}
@@ -33,7 +33,8 @@ const RainChart = (props) => {
 				<ResponsiveContainer width="100%" height={100}>
 					<LineChart data={createDay(forecast)}>
 						<XAxis dataKey="time" name='Time' stroke="#BCCCDC" axisLine={false} tickLine={false} style={{ fontFamily: 'Roboto', fontSize: "12px"}} tickFormatter={(time) => moment(time).format('ha')}/>
-						<Line type="monotone" dot={false} dataKey="value" stroke="#8884d8" strokeWidth={2} />
+						<Line type="monotone" dot={false} dataKey="value" stroke="#8884D8" strokeWidth={2} />
+						<ReferenceDot x={10} y={60} />
 					</LineChart>
 				</ResponsiveContainer>
 			</CardContent>
